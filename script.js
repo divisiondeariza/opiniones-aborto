e3d3ca76aafaca61c3a23eee9b1d4b42a1ca55b3
Emmanuel Ariza <ejariza@gmail.com>
2019-11-28 19:33:10 -0500

    Bare minimum

--- script.js
+++ script.js
@@ -2,12 +2,17 @@
 var data = {}
 d3.csv("data.csv").then(function(csv) {
   //setting keys in data (in a kind of elegant way)
-  (new Set(csv.map( d => d.Medida.replace(/^\s+|\s+$/g, '') )))
+  (new Set(csv.map( d => d.Medida.replace(/^\s+|\s+$/g, '').replace('%', 'percent') )))
   .forEach(function(m){
     data[m] = []
+
+    d3.select("#data-table")
+      .append("div")
+      .attr("id", m.replace(/\s/g, '-'))
+      .attr("class", "row")
   })
   csv.forEach(function(d){
-      data[d.Medida.replace(/^\s+|\s+$/g, '')].push(d);
+      data[d.Medida.replace(/^\s+|\s+$/g, '').replace('%', 'percent')].push(d);
   })
   set_graph()
   console.log(data)
@@ -16,9 +21,12 @@ d3.csv("data.csv").then(function(csv) {
 //Poppin' data in the charts, like a blizzard.
 function set_graph(){
   // set the dimensions and margins of the graph
-  var margin = {top: 20, right: 20, bottom: 30, left: 40},
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+  var margin = {top: 20, right: 20, bottom: 30, left: 50},
+      width = 700 - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom;
+
+  //Create tooltip
+  var tooltip = d3.select("body").append("div").attr("class", "toolTip");
 
   // set the ranges
   var x = d3.scaleBand()
@@ -26,11 +34,15 @@ function set_graph(){
             .padding(0.1);
   var y = d3.scaleLinear()
             .range([height, 0]);
-
+  var ylog = d3.scaleSymlog()
+            .range([height, 0]);
+  var scaleColor = d3.scaleLinear()
+            .domain([0, 9])
+            .range(["#00aae4", "#32a852"])
   // append the svg object to the body of the page
   // append a 'group' element to 'svg'
   // moves the 'group' element to the top left margin
-  var svg = d3.select("body").append("svg")
+  var svg = d3.select("#histogram").append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
@@ -40,6 +52,7 @@ function set_graph(){
   // Scale the range of the data in the domains
   x.domain(data.Freq.map(d => d["Posición Frente al aborto"]));
   y.domain([0, d3.max(data.Freq.map(d => parseInt(d.Valor)))]);
+  ylog.domain([1, d3.max(data.Freq.map(d => parseInt(d.Valor)))]);
 
   // append the rectangles for the bar chart
   svg.selectAll(".bar")
@@ -48,17 +61,56 @@ function set_graph(){
       .attr("class", "bar")
       .attr("x", function(d) { return x(d["Posición Frente al aborto"]) } )
       .attr("width", x.bandwidth())
-      .attr("y", function(d) {return y((parseInt(d.Valor))) })
-      .attr("height", function(d) { return height - y(parseInt(d.Valor)); })
-      .attr("fill", "steelblue");
+      .attr("y", d => ylog((parseInt(d.Valor))) )
+      .attr("height", d =>  height - ylog(parseInt(d.Valor)) )
+      .attr("fill", (d, i) => scaleColor(i) )
+      //now the hover effect
+      .on("mousemove", function(d){
+          tooltip
+            .style("left", x(d["Posición Frente al aborto"]) + x.bandwidth() + 27 + "px")
+            .style("top", ylog(parseInt(d.Valor)) + 110 + "px")
+            .style("display", "inline-block")
+            .html((parseInt(d.Valor)));
+      })
+      .on("mouseout", function(d){ tooltip.style("display", "none");})
+      .on("click", function(d, i){
+        d3.select("#wordcloud")
+          .attr("src", "clouds/wc_" + d["Posición Frente al aborto"] + ".html")
+
+          for (var key in data) {
+            console.log(key);
+            if (data.hasOwnProperty(key)) {
+              console.log(key.replace(/\s/g, '-'));
+                d3.select("#data-table #" + key.replace(/\s/g, '-'))
+                  .html(
+                    "<div class='col-4'>" +
+                      data[key][i].Medida +
+                    "</div>"+
+                    "<div class='col-8'>" +
+                      data[key][i].Valor +
+                    "</div>"
+                  )
+
+            }
+        }
+      });
 
   // add the x Axis
   svg.append("g")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+      .html((d, i) => i + 1)
+      // .attr("y", 50)
+      // .attr("x", 20)
+      // .attr("dy", ".35em")
+      // .attr("transform", "rotate(-30)")
+      // .style("text-anchor", "start");
 
   // add the y Axis
   svg.append("g")
-      .call(d3.axisLeft(y))
+      .call(d3.axisLeft(ylog)
+              .tickValues([1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000,
+                          d3.max(data.Freq.map(d => parseInt(d.Valor)))]))
 
 }
